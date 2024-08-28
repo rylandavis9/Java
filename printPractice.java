bb055c74af410c23c610745d18e7d6cf542a264b
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2024-08-28 10:38:02 -0500

    Create printPractice.java

---
+++ printPractice.java
@@ -0,0 +1,11 @@
+/*
+Rylan Davis 8/22/24
+*/
+public class printPractice{
+    public static void main(String[] args){
+        //Type your code below
+        System.out.println("Here Are Some Of my Favorite Things:");
+        //Seperate for better readability
+        System.out.println("\n\t-Running\n\t-Photography\n\t-Cooking\n\t-Coding\n\t-Eating");
+    }
+}
