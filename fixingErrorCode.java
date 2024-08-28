c5eea637e4601bda64b43bb8cdfe39f3b751af34
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2024-08-28 10:39:27 -0500

    Create fixingErrorCode.java

---
+++ fixingErrorCode.java
@@ -0,0 +1,8 @@
+/*
+Rylan Davis 8/22/24
+*/
+public class main{
+    public static void main(String[] args){
+       System.out.println("\n\"Just because something doesn’t do what you planned it to do doesn’t mean it’s useless.\" - Thomas Edison (Inventor)");
+    }
+}
