4036e67a30a93c0b67230d2800392850938b2bf1
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2024-08-28 10:40:20 -0500

    Create data types.java

---
+++ data types.java
@@ -0,0 +1,29 @@
+/*
+Rylan Davis 8/28/24
+*/
+public class Mathematical{
+    public static void main(String[] args){
+        double width = 5.5;
+        //create another double variable named height and set it to 3.4
+        double height = 3.4;
+        double area = width * height;
+        //print out the answer area
+        System.out.println(area);
+        //create two integer variables and assign and int value to them
+        int value1 = 3;
+        int value2 = 5;
+        //add the values
+        int sum = value1 + value2;
+        //print the answer
+        System.out.println(sum);
+        //using modules find the remainder (order does not matter)
+        //There is nothing I have divided so i just assumed you wanted me to do it with the integers I made.
+        int remainder = value1 % value2;
+        // print the answer    
+        System.out.println(remainder);
+        
+        //print name as said in video
+        String lastName = "Davis";
+        System.out.println(lastName);
+    }
+}
